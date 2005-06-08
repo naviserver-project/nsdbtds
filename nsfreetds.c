f854a63f1be4f022dc0b78dd57f75d5c9a232118
Vlad Seryakov <vlad@crystalballinc.com>
2005-06-08 20:03:44 +0000

    Change license and wording in README files

--- nsfreetds.c
+++ nsfreetds.c
@@ -1,9 +1,29 @@
-/*
+/* 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1(the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/.
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis,WITHOUT WARRANTY OF ANY KIND,either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Alternatively,the contents of this file may be used under the terms
+ * of the GNU General Public License(the "GPL"),in which case the
+ * provisions of GPL are applicable instead of those above.  If you wish
+ * to allow use of your version of this file only under the terms of the
+ * GPL and not to allow others to use your version of this file under the
+ * License,indicate your decision by deleting the provisions above and
+ * replace them with the notice and other provisions required by the GPL.
+ * If you do not delete the provisions above,a recipient may use your
+ * version of this file under either the License or the GPL.
+ *
+ * Author Vlad Seryakov vlad@crystalballinc.com
+ * 
  * FreeTDS internal driver for NaviServer 4.x
  *
  * Requires FreeTDS version 0.64+
- *
- * Vlad Seryakov vlad@crystalballinc.com
  *
  * Derived from FreeTDS driver by Dossy <dossy@panoptic.com>
  */
@@ -229,7 +249,6 @@ Db_GetRow(Ns_DbHandle *handle, Ns_Set *row)
     rc = tds_process_tokens(GET_TDS(handle),&resulttype,&computeid,TDS_STOPAT_ROWFMT|TDS_RETURN_DONE|TDS_RETURN_ROW|TDS_RETURN_COMPUTE);
     if(rc != TDS_SUCCEED && rc != TDS_NO_MORE_RESULTS) {
        Ns_Log(Error,"Db_GetRow(%s): tds_process_row_tokens: %d",handle->datasource,rc);
-       Db_Cancel(handle);
        return NS_ERROR;
     }
     if(rc == TDS_NO_MORE_RESULTS || 
